1229c9a5ec32a6ae81d0c2d78704c9d2b1c24798
saiganesh-sristla <saiganesh3108@gmail.com>
2024-01-14 23:09:45 +0530

    corrected width

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 const VideoCard = ({ info }) => {
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
-  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
-
-  let cardWidth = "24rem";
-
-  if (!isMenuOpen) {
-    cardWidth = "22rem";
-  }
 
   return (
-    <div className={`m-2 w-[${cardWidth}] shadow-lg`}>
+    <div className={`m-2 w-96 shadow-lg`}>
       <img
         className="w-full rounded-xl"
         alt="Thumbnail"
